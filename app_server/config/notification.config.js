070c6ded5d2ea4df3c2e655b7e84c5a2ac0e6786
yayayay <khangds218@gmail.com>
2017-06-13 09:26:39 +0700

    Schedule notification

--- app_server/config/notification.config.js
+++ app_server/config/notification.config.js
@@ -14,7 +14,8 @@ var options = {
 
 var apnProvider = new apn.Provider(options);
 
-let deviceToken = "0a6fc813ca35b758c730fe1efe9e172c0a4ce63b93630c906290f97d2a9da8c3";
+// let deviceToken = "0a6fc813ca35b758c730fe1efe9e172c0a4ce63b93630c906290f97d2a9da8c3";
+let deviceToken = '5f1737e692702e0c49fc607f5d169e9fd18c92612206695096fdff8ec0495111';
 var note = new apn.Notification();
 
 //  Notification Content
@@ -24,28 +25,28 @@ note.sound = "default";
 
 var rule = new schedule.RecurrenceRule();
 
-note.body = 'Link ';
-note.title = 'Message ';
-rule.hour = 0;
-rule.minute = 25;
+// note.body = 'Link ';
+// note.title = 'Message ';
+// rule.hour = 8;
+// rule.minute = 56;
 //  Config Pushing Time Daily
-// Notifications.find(function (err, notification) {
-//     if (err)
-//         console.log(err);
-//     else {
-//         for(var i=0; i<notification.length; i++) {
-//             note.body = 'Link ' + notification[i].link;
-//             note.title = 'Message ' + notification[i].message;
-//             rule.hour = notification[i].hour;
-//             rule.minute = notification[i].minute;
-//             schedule.scheduleJob(rule, function () {
-//                 apnProvider.send(note, deviceToken).then(function (result) {
-//                     console.log(result);
-//                 });
-//             });
-//         }
-//     }
-// });
+Notifications.find(function (err, notification) {
+    if (err)
+        console.log(err);
+    else {
+        for(var i=0; i<notification.length; i++) {
+            note.body = 'Link ' + notification[i].link;
+            note.title = 'Message ' + notification[i].message;
+            rule.hour = notification[i].hour;
+            rule.minute = notification[i].minute;
+            schedule.scheduleJob(rule, function () {
+                apnProvider.send(note, deviceToken).then(function (result) {
+                    console.log(result);
+                });
+            });
+        }
+    }
+});
 
 // schedule.scheduleJob(rule, function () {
 //     apnProvider.send(note, deviceToken).then(function (result) {
